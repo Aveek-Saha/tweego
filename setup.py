ba2c65ea4887e71200f1943444c016fe4f634915
Aveek Saha <aveek.s98@gmail.com>
2021-10-04 20:20:30 +0530

    Add setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='tweego',
+    version='0.0.1',
+    author="Aveek Saha",
+    author_email="aveek.s98@gmail.com",
+    url='https://github.com/Aveek-Saha/tweego',
+    description="Generate ego networks for users from Twitter.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages= setuptools.find_packages(),
+    include_package_data=True,
+    py_modules=['tweego'],
+    install_requires=[
+        'Click',
+        'networkx',
+        'TwitterAPI',
+        'matplotlib',
+        'tqdm'
+    ],
+    entry_points={
+        'console_scripts': [
+            'tweego = tweego:cli',
+        ],
+    },
+)
